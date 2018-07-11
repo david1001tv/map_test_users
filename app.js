9ee6678a71cd3f93360bc19658685a0a443320fb
david1488tv <37955716+david1488tv@users.noreply.github.com>
2018-07-11 22:02:29 +0300

    Изменил порт на 3000 везде для избежания ошибок

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
  * Express configuraion
  */
 app.set('host', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
-app.set('port', config.port || 8080);
+app.set('port', config.port || 3000);
 app.use(compression());
 app.use(morgan(':date[web] :method :url :status :response-time ms :remote-addr'));
 app.use(bodyParser.json());
